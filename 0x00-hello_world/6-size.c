4fb601ab0b4a0a8122256fa4343d7439f3901308
Theodora Aidoo <theodoraaidoo@gmail.com>
2023-02-16 14:34:01 -0800

    Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - functions that prints the size of various types on the computer
+ * by Theodora Aidoo
+ * Return: o on success
+ */
+int main(void)
+{
+	printf("Size of a char:%lu byte(s)", sizeof(char));
+	printf("Size of int:%lu byte(s)", sizeof(int));
+	printf("Size of a long int:%lu byte(s)", sizeof(long int));
+	printf("Size of a long long int:%lu byte(s)", sizeof(long long int));
+	printf("Size of a float:%lu byte(s)", sizeof(float));
+	return (o);
+}
