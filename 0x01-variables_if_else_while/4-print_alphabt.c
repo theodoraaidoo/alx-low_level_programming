3e8788de064357e7665a9dc33c33879ac837fb87
Theodora Aidoo <theodoraaidoo@gmail.com>
2023-02-17 12:45:04 -0800

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,8 @@ int main(void)
 	c = 'a';
 	while
 		(c <= 'z') {
-			if ((c != 'q' && c != "e") && c <= "z")
-					putchar(c);
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
 			c++;
 		}
 	putchar('\n');
