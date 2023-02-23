bb2af0566d247660a543c7909ef5e663e8c03e20
Theodora Aidoo <theodoraaidoo@gmail.com>
2023-02-23 15:13:49 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a number is uppercase
+ * @x: The number to be checked
+ * Return: 1 for upper letter 0 for anything else
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
