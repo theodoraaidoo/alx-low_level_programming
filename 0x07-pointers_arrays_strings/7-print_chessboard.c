540ed2ae1027885b7f648936db29278a95409d11
Theodora Aidoo <theodoraaidoo@gmail.com>
2023-03-06 05:39:52 -0800

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
+	int i;
 	int j;
-	int k;
 
-	for (j = 0; j < 8; j++)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
-			_putchar(a[j][k]);
+			_putchar(a[i][j]);
 		_putchar('\n');
 	}
 }
