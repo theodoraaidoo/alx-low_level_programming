e2da3b9e4be98aa5b8ba3d65a2c171bba31695ba
Theodora Aidoo <theodoraaidoo@gmail.com>
2023-03-08 06:17:38 -0800

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,40 +1,33 @@
 #include "main.h"
 
+int actual_sqrt_recursion(int n, int i);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: the number whose square root is to be calculated
+ * @n: number to calculate the square root of
  *
- * Return: the natural square root of n,
- * or -1 if n does not have a natural square root
+ * Return: the resulting square root
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
-		return (-1); /* return -1 for invalid input */
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (n); /* base case: square root of 0 or 1 is itself */
-	}
-	else
-	{
-		/* recursive case */
-		int i;
+		return (-1);
+	return (actual_sqrt_recursion(n, 0));
+}
 
-		for (i = 1; i * i < n; i++)
-		{
-			/* do nothing */
-		}
-
-		if (i * i == n)
-		{
-			return (i); /* found the natural square root */
-		}
-		else
-		{
-			return (-1); /* no natural square root exists for n */
-		}
-	}
+/**
+ * actual_sqrt_recursion - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
 }
